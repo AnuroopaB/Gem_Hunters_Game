5d94e255527d0a97b19a596093769aae93042eef
Anuroopa <Anuroopa@ROOZ>
2024-02-21 12:42:05 -0500

    Code changes for updating the player's position using input direction.

--- Gem_Hunters_Game/Player.cs
+++ Gem_Hunters_Game/Player.cs
@@ -14,7 +14,26 @@ namespace Assignment2
         {
             int row = position.X;
             int column = position.Y;
-            
+            if (direction == 'U' || direction == 'u')
+            {
+                row -= 1;
+                position = new Position(row, column);
+            }
+            else if (direction == 'D' || direction == 'd')
+            {
+                row += 1;
+                position = new Position(row, column);
+            }
+            else if (direction == 'R' || direction == 'r')
+            {
+                column += 1;
+                position = new Position(row, column);
+            }
+            else
+            {
+                column -= 1;
+                position = new Position(row, column);
+            }
         }
     }
 }
